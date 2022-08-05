3c16d3ca256a703b31b85a148f74bba66563f5c6
Rion Tao <ziyu.tao@outlook.com>
2022-08-05 16:24:10 +0800

    feat: initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+    return (<div>
+        Hello Electron React Typescript
+    </div>)
+}
+
+export default App;
---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './app';
+
+
+ReactDOM.render(<App />, document.body);
