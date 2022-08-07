9763f5d9d3a40c5e7af7a219e35f5dd8f6defa10
Rion Tao <ziyu.tao@outlook.com>
2022-08-07 12:27:41 +0800

    refactor: move to isolated file

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useMemo, useRef } from "react";
-import * as PIXI from "pixi.js";
-import * as U from "./utils";
-import { Slime } from "../assets/ShremAnim/AnimatedImage";
+import React, { useMemo } from "react";
 import './app.less';
+import Pixi from "./pixi/Pixi";
 
 function App() {
   const canvas = useMemo(() => <Pixi />, []);
   return (<div>
     {canvas}
   </div>)
-}
-
-const PIXIConfig = {
-  width: U.WINDOW_WIDTH,
-  height: U.WINDOW_HEIGHT,
-  backgroundColor: 0x66ccff,
-  resolution: window.devicePixelRatio,
-} as const;
-
-function Pixi() {
-  const app = new PIXI.Application(PIXIConfig);
-  const container = new PIXI.Container();
-  app.stage.addChild(container);
-
-  const texture = PIXI.Texture.from(Slime[0]);
-  container.addChild(new PIXI.Sprite(texture));
-  container.x = app.screen.width * 0.5;
-  container.y = app.screen.height * 0.5;
-
-  app.ticker.add((delta) => {
-    container.rotation -= 0.01 * delta;
-  });
-
-  const ReactContainer = useRef(null);
-  useEffect(() => {
-    ReactContainer.current.innerHTML = "";
-    ReactContainer.current.append(app.view);
-  }, [ReactContainer, app]);
-  return <div className="main-canvas" ref={ReactContainer} />
 }
 
 export default App;
---
+++ src/app/pixi/Pixi.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from "react";
+import * as PIXI from "pixi.js";
+import * as U from "../utils";
+import { Slime } from "../../assets/ShremAnim/AnimatedImage";
+
+const PIXIConfig = {
+    width: U.WINDOW_WIDTH,
+    height: U.WINDOW_HEIGHT,
+    backgroundColor: 0x66ccff,
+    resolution: window.devicePixelRatio,
+} as const;
+
+function Pixi() {
+    const app = new PIXI.Application(PIXIConfig);
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
+
+    const texture = PIXI.Texture.from(Slime[0]);
+    container.addChild(new PIXI.Sprite(texture));
+    container.x = app.screen.width * 0.5;
+    container.y = app.screen.height * 0.5;
+
+    app.ticker.add((delta) => {
+        container.rotation -= 0.01 * delta;
+    });
+
+    const ReactContainer = useRef(null);
+    useEffect(() => {
+        ReactContainer.current.innerHTML = "";
+        ReactContainer.current.append(app.view);
+    }, [ReactContainer, app]);
+    return <div className="main-canvas" ref={ReactContainer} />
+}
+
+export default Pixi;
