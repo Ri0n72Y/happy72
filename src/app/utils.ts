c0c3887a8b962e78cce5d9ce7631d7e8e02467f7
Rion Tao <ziyu.tao@outlook.com>
2022-08-08 01:15:56 +0800

    feat: add const

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -4,3 +4,5 @@ export const CANVAS_HEIGHT = 960;
 export const CELL_ROWS = 20;
 export const CELL_COLS = 32;
 export const CELL_SIZE = 48;
+
+export const BUILDING_RADIUS = Math.sqrt(CELL_SIZE ** 2 * 2);
