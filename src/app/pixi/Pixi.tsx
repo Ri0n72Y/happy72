e7720d0dfc667c4e7f956551419495a23e78818a
WhirlpoolSeaweed <yorktown1781@163.com>
2022-08-08 14:16:46 +0800

    feat: animAsync

--- src/app/pixi/Pixi.tsx
+++ src/app/pixi/Pixi.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
+import { sound } from '@pixi/sound';
 import * as U from "../utils";
 import * as UI from "./UI";
-import * as AG from "./AnimGenerator";
 import { app } from "electron";
+import { init, AnimSlimee, AnimBuilding, AnimSunshine } from "./AnimGenerator";
+import * as AG from "./AnimGenerator";
+import { Vec2 } from "../utils/gameProps.typed"
 
 const PIXIConfig = {
     width: U.CANVAS_WIDTH,
@@ -14,8 +17,24 @@ const PIXIConfig = {
 
 function Pixi() {
     const app = new PIXI.Application(PIXIConfig);
+    const sources = [
+        { name: 'slime', url: '../../assets/Shrem_Blue/spritesheet.json' },
+        { name: 'sunshine', url: '../../assets/Props/Sunshine/spritesheet.json' }];
+    const functions = [
+        () => AnimSlimee(app, "Blue"),
+        // () => AnimBuilding(app, "CABIN", pos),
+        // () => AnimBuilding(app, type, pos),
+        () => AnimSunshine(app)];
+    init(app, functions, sources);
+
+
+
     UI.DrawCells(app);
-    AG.AnimSlimee(app, 'Blue');
+    // AG.AnimSlimee(app, 'Blue');
+    // const sounds = sound.add('mysound', '../../assets/sounds/Music/health.mp3');
+    // sounds.play('mysound');
+    // app.loader.reset();
+    // AG.AnimBuilding();
     // AG.AnimSunshine(app);
 
     const ReactContainer = useRef(null);
